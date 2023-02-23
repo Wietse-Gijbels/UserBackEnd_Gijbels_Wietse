a5b49aaa92f2f75a5245979714034e84a96664a5
Wietse-Gijbels <wietse.gijbels@student.ucll.be>
2023-02-23 10:21:14 +0100

    rest controller made

---
+++ src/main/java/demo/UserRestController.java
@@ -0,0 +1,41 @@
+package demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://127.0.0.1:3000")
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/oldest")
+    public User getOldestUser() {
+        return userService.getOldestUser();
+    }
+
+    @GetMapping("/search/olderthan")
+    public List<User> searchUsersWithAgeOlderThan(@RequestParam("age") int age) {
+        return userService.getUsersWithAgeOlderThan(age);
+    }
+
+    @GetMapping("/search/{name}")
+    public User searchUserWithName(@PathVariable("name") String name) {
+        return userService.getUserWithName(name);
+    }
+
+}
