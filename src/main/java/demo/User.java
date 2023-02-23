f8fe72334bc78204a9bacbd457f4c1940e18cbba
Wietse-Gijbels <wietse.gijbels@student.ucll.be>
2023-02-23 09:48:43 +0100

    user test complete

--- src/main/java/demo/User.java
+++ src/main/java/demo/User.java
@@ -3,16 +3,25 @@ package demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.h2.result.ResultColumn;
+
 public class User {
 
     private String name;
     private int age;
     private List<Integer> membershipYears = new ArrayList<Integer>();
+    private String email;
+    private String password;
 
-    public User(String name, int age) {
+    public User(String name, int age, String email, String password) {
         this.name = name;
         if (age >= 0) 
             this.age = age;
+        if(email.contains("@")){
+            this.email=email;
+        }
+        if(password.trim()==""){this.password="@$-t&%#";}
+        else{this.password="@$-"+password.trim()+"&%#";}
     }
 
     public int countMembershipYearsAfter1999 () {
@@ -40,4 +49,43 @@ public class User {
         return name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getFirstMembershipYear(){
+        if(membershipYears.size()==0){
+            return 0;
+        }
+        else{Integer result=membershipYears.get(0);
+        for(Integer year:membershipYears){
+            if(year<result){result=year;}
+        }
+        return result;
+        
+    }
+    }
+
+    public int getNumberOfMembershipYearsIn2000(){
+        int result=0;
+        for(Integer year:membershipYears){
+            if(year>=2000&&year<3000){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public boolean isPasswordCorrect(String pass){
+        if(pass.equals(password))return true;
+        else return false;
+    }
+
+    public String toString(){
+        return name+" is "+age+" years old and has as email "+email;
+    }
 }
